bc89c08355d04652efef69b02bc393b2da14a72e
Kathy Lambert <Kathy.Lambert@LotusDesignsAgency.com>
2017-11-20 18:46:53 -0500

    Created game logic Unit 9

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
-var cardOne = "queen";
+var cards = ['queen', 'queen', 'king', 'king'];
 
-var cardTwo = "queen";
+var cardsInPlay = [];
 
-var cardThree = "king";
+/*var cardOne = cards[0];*/
 
-var cardFour = "king";
+/*cardsInPlay.push(cardOne);*/
 
-console.log("User flipped " + cardOne)
+/*var cardTwo = cards[3];*/
 
-console.log("User flipped " + cardThree)
+/*cardsInPlay.push(cardTwo);*/
+var checkForMatch = function () {
+
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+        } else {
+        console.log("Sorry, try again.");
+        }
+}
+
+var flipCard = function (cardId) 
+{ console.log('User flipped ' + cards[cardId])
+    cardsInPlay.push(cards[cardId])
+    checkForMatch()
+}
+
+flipCard(0)
+flipCard(2)
+
+/*if (cardsInPlay.length === 2) {
+
+    if (cardsInPlay[0] === cardsInPlay[1]);
+
+} {
+        
+    alert("You found a match!");
+        
+    }  {
+    
+    alert("Sorry, try again.");
+
+}*/
